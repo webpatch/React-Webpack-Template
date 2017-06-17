39947379232637bb6b9c980b09e920259464d558
webpatch <webpatch@sina.com>
2017-06-17 09:59:48 +0800

    update

---
+++ bootstrap.js
@@ -0,0 +1,41 @@
+module.exports = {
+  prompt: [
+    {
+      type: 'confirm',
+      name: 'redux',
+      message: 'Use redux?'
+    },
+    {
+      type: 'confirm',
+      name: 'router',
+      message: 'Use react router?'
+    },
+    {
+      type: 'confirm',
+      when: 'router',
+      name: 'cssm',
+      message: 'Use react css moudules?'
+    },
+    {
+      type: 'confirm',
+      name: 'testing',
+      message: 'Is need be testing?'
+    },
+    {
+      type: 'confirm',
+      name: 'install',
+      message: 'Install all dependence right now?'
+    },
+  ],
+  completeMessage: 'To get started:\n\n cd <%=destDirName%>\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack',
+  filter: {
+    'test/*': 'testing',
+    'dev/bage.js': 'testing',
+    'dev/karma.conf.js': 'testing',
+    'src/model/*': 'redux',
+    'src/warp.jsx.ejs': 'redux|router',
+  },
+  skip: [
+    'src/part/*',
+  ]
+};
