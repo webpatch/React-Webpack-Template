cbcc916f5e60aa1a85cfd3279e01ab1e3a0b3c6c
webpatch <webpatch@sina.com>
2017-06-21 11:10:01 +0800

    replace with babel-plugin-react-css-modules-modify

--- template/src/part/wrap-router.jsx
+++ template/src/part/wrap-router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router';
 import App from './app';
 import About from './views/about';
 import User from './views/user';
