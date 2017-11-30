8e2d3f060968a404f46178717b6fa71c041d34ac
webpatch <webpatch@sina.com>
2017-11-30 11:06:51 +0800

    add eslint rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     "jsx-a11y/href-no-hash": 0,
     "react/forbid-prop-types": 0,
     "jsx-a11y/no-static-element-interactions": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
     'max-len': ["error", 120],
   },
   "extends": "airbnb",
